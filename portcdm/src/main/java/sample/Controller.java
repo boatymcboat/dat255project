8531fd77098f58a8a0d2f507c30169e2b0b12e2f
Aron Sai <aron.sai@gmail.com>
2017-04-26 14:07:15 +0200

    Basic Hello World Window & Working Connection w/ VirtualBox

--- portcdm/src/main/java/sample/Controller.java
+++ portcdm/src/main/java/sample/Controller.java
@@ -1,7 +1,35 @@
 package sample;
 
-import se.viktoria.util.Configuration;
+import se.viktoria.stm.portcdm.connector.common.*;
+import se.viktoria.util.*;
+
+import java.util.Map;
+import java.util.function.Predicate;
 
 public class Controller {
-    //KOD HÄR XDDD
+    public static void connect(){
+        Configuration configuration;
+        // * Read the configuration file
+
+        configuration = new Configuration("config.conf","/",new Predicate<Map.Entry<Object,Object>>() {
+            @Override
+            public boolean test(Map.Entry<Object,Object> objectObjectEntry) {
+                return !objectObjectEntry.getKey().toString().equals("pass");
+            }
+        });
+        configuration.reload();
+
+
+        // ** Create a submission service and add connectors
+        SubmissionService submissionService;
+        submissionService = new SubmissionService();
+        submissionService.addConnectors(configuration);
+
+        // ** Create a list of port call messages (somehow)
+        // List<PortCallMessage> messages;
+        // messages = createMessages();
+        // ** Submit the messages
+        //submissionService.submitUpdates(messages);
+    }
+
 }
