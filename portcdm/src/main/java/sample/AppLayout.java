4becf944c08dc0013daa379f79a716dc0e32a613
teomar <teodorm@student.chalmers.se>
2017-04-28 21:23:57 +0200

    Added action handler for button.
    Added drop down menu with text changing depending on option chosen when button is clicked.

--- portcdm/src/main/java/sample/AppLayout.java
+++ portcdm/src/main/java/sample/AppLayout.java
@@ -1,5 +1,9 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.*;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -14,27 +18,66 @@ import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.geometry.*;
+import javafx.scene.text.*;
+import java.lang.*;
+import java.awt.event.*;
 public class AppLayout {
 
     public static void Setup_App(Stage primaryStage){
         primaryStage.setTitle("Professional Ship Agent Software 2018");
 
-        Button btn = new Button();
-        btn.setText("Start Agent Application");
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        Scene scene = new Scene(root, 800, 500);
-        scene.setRoot(root);
+
+        final Text sceneTitle = new Text("Welcome to the portCDM agent application");
+        sceneTitle.setFont(Font.font(26));
+
+        Button btn = new Button("Start Agent Application");
+
+        final ComboBox choices = Create_Drop_Down_Menu(new String []{"option1", "option2", "option3"});
+        HBox choiceBox = new HBox(10);
+        choiceBox.setAlignment(Pos.BOTTOM_CENTER);
+        choiceBox.getChildren().add(choices);
+        HBox button =  new HBox (10);
+        button.setAlignment(Pos.BOTTOM_CENTER);
+        button.getChildren().add(btn);
+        GridPane grid = Setup_Grid();
+        Scene scene = new Scene(grid, 800, 500);
+        grid.add(choiceBox, 0,5);
+        grid.add(sceneTitle, 0, 0);
+        grid.add(button,0,15);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        btn.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){
+                if(choices.getValue() != null && !choices.getValue().toString().isEmpty()){
+                    if (choices.getValue().toString().equals("option1")){
+                        sceneTitle.setText("You have chosen option1");
+                    }
+                    else if (choices.getValue().toString().equals("option2")){
+                        sceneTitle.setText("You have chosen option2");
+                    }
+                    else if (choices.getValue().toString().equals("option3")){
+                        sceneTitle.setText("You have chosen option3");
+                    }
+                }
+            }
+        });
+        primaryStage.show();
+
+    }
+
+    public static GridPane Setup_Grid (){
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
+        return grid;
+    }
 
-        primaryStage.setScene(scene);
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(true);
-        primaryStage.show();
-
+    public static ComboBox Create_Drop_Down_Menu(String[] optionsArray){
+        ObservableList<String> choices = FXCollections.observableArrayList();
+        choices.addAll(optionsArray);
+        return new ComboBox(choices);
     }
 }
