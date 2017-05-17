28081eb8f4a1365366aaee2744dff7263e3de88b
anton1904 <anton.erik.alexandersson@gmail.com>
2017-05-17 10:47:59 +0200

    Metoder för att skicka ServiceState och LocationState

--- portcdm/src/test/java/model/MessageSenderTest.java
+++ portcdm/src/test/java/model/MessageSenderTest.java
@@ -1,8 +1,12 @@
 package model;
 
-import eu.portcdm.messaging.PortCallMessage;
+import eu.portcdm.dto.LocationTimeSequence;
+import eu.portcdm.messaging.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by arono on 2017-05-15.
  */
 class MessageSenderTest {
+
     @BeforeAll
     static void checkConnection() {
         assertTrue(ApiConfig.isOnline(),"Server is unreachable!");
@@ -29,4 +34,21 @@ class MessageSenderTest {
         assertTrue(sender.sendMessage(pcm), "Could not send PCM");
     }
 
+    @Test
+    void sendLocationState() {
+        MessageSender sender = new MessageSender();
+        PortCallManager manager = new PortCallManager();
+        sender.sendLocationState(manager.getActiveCall(), LocationTimeSequence.ARRIVAL_TO,
+                LogicalLocation.ANCHORING_AREA, LogicalLocation.BERTH,
+                ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT), TimeType.ESTIMATED);
+    }
+
+    @Test
+    void sendServiceState() {
+        MessageSender sender = new MessageSender();
+        PortCallManager manager = new PortCallManager();
+        sender.sendServiceState(manager.getPortCall(1), ServiceObject.CARGO_OPERATION, ServiceTimeSequence.COMMENCED,
+                LogicalLocation.ANCHORING_AREA ,ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT),
+                TimeType.ESTIMATED);
+    }
 }
