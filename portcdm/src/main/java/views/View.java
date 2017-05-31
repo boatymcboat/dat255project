488155143c61a22d7a29eba79ec3b438ca2b46f7
Aron Sai <aron.sai@gmail.com>
2017-05-31 18:55:19 +0200

    Added JavaDocs to View class

--- portcdm/src/main/java/views/View.java
+++ portcdm/src/main/java/views/View.java
@@ -7,14 +7,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Simple class to load the initial FXML file.
+ */
 public class View {
     private Stage primaryStage;
 
+    /**
+     * Creates an instance, saves the Stage in the view and loads the default FXML file
+     *
+     * @param primaryStage the Stage for which to load the FXML file
+     * @throws IOException if the file is not found
+     */
     public View(Stage primaryStage) throws IOException {
         this.primaryStage = primaryStage;
         setupView();
     }
 
+    // Loads the MainView.fxml file and sets the Stage to use it
     private void setupView() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
         primaryStage.setScene(new Scene(root));
