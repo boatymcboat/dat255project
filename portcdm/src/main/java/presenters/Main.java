489354c7fe3dfbc6fc3faa8adcf08cb1ba2297e0
Aron Sai <aron.sai@gmail.com>
2017-05-31 18:25:15 +0200

    Cleanup in Main

--- portcdm/src/main/java/presenters/Main.java
+++ portcdm/src/main/java/presenters/Main.java
@@ -7,22 +7,11 @@ import views.View;
 public class Main extends Application {
 
     public static void main(String[] args) {
-
-        // För att skriva ut senaste Statement för Arrival_Vessel_PilotBA (hårdkodad vilken portcall det är)
-        /*PortCallManager manager = new PortCallManager();
-        PortCall call = manager.getActiveCall();
-        System.out.println(call.getVessel());*/
-        /*StatementReader sreader = new StatementReader(call);
-        System.out.println(sreader.getStatement("Arrival_Vessel_PilotBA")); */
-
         launch(args);
-
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         new View(primaryStage);
-
     }
 }
